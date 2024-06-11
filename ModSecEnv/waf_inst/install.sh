3d2a474815c503e491cbb501a95d004ad5ac942d
wiauxb <bastien.wiaux@gmail.com>
2024-06-11 11:38:12 +0200

    Setup Docker compose env

---
+++ ModSecEnv/waf_inst/install.sh
@@ -0,0 +1,17 @@
+export waf_harden_opt_autoupdate=yes
+export waf_harden_opt_ipv6d=yes
+export waf_harden_opt_noautoupdate=no
+export waf_harden_opt_sshd=no
+export waf_inst_machine_id=-
+export waf_inst_opt_acme=no
+export waf_inst_opt_anacron=yes
+export waf_inst_opt_conf_common=no
+export waf_inst_opt_conf_default=no
+export waf_inst_opt_elastic=no
+export waf_inst_opt_htdocs=yes
+export waf_inst_opt_nologrotate=no
+export waf_inst_opt_noreboot=no
+export waf_inst_opt_ntp=-
+export waf_inst_opt_stop=yes
+export waf_inst_opt_tz=Europe/Brussels
+./approach-waf-ai-install.sh
